e2d8d63b6983f19cf89b53bdba545dd0d8087779
unknown <10558894@CS514-01.ad.uvu.edu>
2015-09-08 09:52:47 -0600

    comiting - convention and if statements

---
+++ Unity-Scripting-fall/Assets/Variablesandfunctions.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Variablesandfunctions : MonoBehaviour
+{   
+	int number = 10;
+	
+	
+	void Start ()
+	{
+		number = MultiplyByTwo(number);
+		Debug.Log (number);
+	}
+	
+	
+	int MultiplyByTwo (int number)
+	{
+		int ret;
+		ret = number * 2;
+		return ret;
+	}
+}
---
+++ Unity-Scripting-fall/Assets/conventionsandsyntax.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class conventionsandsyntax : MonoBehaviour
+{
+	void Start ()
+	{
+		Debug.Log(transform.position.x);
+		
+		if(transform.position.y <= 5f)
+		{
+			Debug.Log ("Here we go again!");
+		}
+	}
+}
